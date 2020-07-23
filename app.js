b239da4284d5369b5a5bdd31c595687b0ed39b04
Victor Chirino <vicchirino@gmail.com>
2020-07-23 12:55:08 -0300

    udpate header for demo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/demo', (req, res) => {
-	res.set('X-full-stack, 4life')
+	res.set('X-full-stack', '4life')
 	res.status(418)
 	res.send('I prefer coffee')
 })
